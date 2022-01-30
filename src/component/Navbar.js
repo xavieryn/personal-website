d8dcdcc41b4223d5ddcb6d9a8273c79b439b11a2
xavieryn <61944157+xavieryn@users.noreply.github.com>
2022-01-30 15:27:48 -0800

    adds some more nav bar ui

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { navLinks } from "../data";
-import { Link, Box, UnorderedList, ListItem } from "@chakra-ui/layout";
+import { Link, Box, UnorderedList, ListItem, Heading } from "@chakra-ui/layout";
 import styles from '../../styles/Home.module.css'
-import styled from "styled-components";
 
 export default function Navbar() {
     console.log('pp')
     return (
         <Box className={styles.navBarContainer}>
             <Box>
-                <h3>Example</h3>
+                <Heading fontSize={[".75rem", "1.2rem"]} as="h2">Xavier Nishikawa</Heading>
             </Box>
             <Box className={styles.navBarRight}>
                 {navLinks.map((link, index) => {
                 return (
                     <UnorderedList className={styles.ul}>
                        <Link href={link.path} >
-                            <ListItem key={index} className={styles.listItem}>{link.name}</ListItem>
+                            <ListItem key={index} className={styles.listItem}>
+                                <Heading fontSize={[".75rem", "1.2rem"]} as="h2">{link.name}</Heading>
+                                </ListItem>
                         </Link>
+
                     </UnorderedList>
                  );
                 })}
