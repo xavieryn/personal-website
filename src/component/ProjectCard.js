ea16a8213dfd1a0f48f709c80ca4ea8e4923728e
xavieryn <61944157+xavieryn@users.noreply.github.com>
2022-02-07 10:34:37 -0800

    some flex box, start to make box for actual code and make responsive

--- src/component/ProjectCard.js
+++ src/component/ProjectCard.js
@@ -1,6 +1,7 @@
-import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { Box, Heading, Image, Text, Link} from '@chakra-ui/react';
 import React from 'react';
 import styles from '../../styles/Home.module.css'
+import nextLink from 'next/link';
 
 
 const ProjectCard = ( { props }) => {
@@ -9,18 +10,22 @@ const ProjectCard = ( { props }) => {
     console.log(props.items)
     console.log(props.items.imagegallery)
     return (
-        <Box>
+        <Box className={styles.ProjectCard}>
             {/* {props.items.map((project) => { */}
             {props.items.map((item) => transformContent(item)).map(project => {
                 
                 return (
                     // card 
-                    <Box key={project.key} className={styles.ProjectCard}> 
+                    <Box key={project.key} > 
                         <Heading>
                             {project.title}
                         </Heading>
-                        <Image src={ "https://berowra.xavier.deta.app" + "/file/" + project.content.ImageGallery.value[0]}/> 
-                        <Text>{project.content.Body.value}</Text>
+                        <nextLink>
+                            <Link>
+                                <Image src={ "https://berowra.xavier.deta.app" + "/file/" + project.content.ImageGallery.value[0]}  w={'100%'}/> 
+                            </Link>
+                        </nextLink>
+                        {/* <Text>{project.content.Body.value}</Text> */}
                     </Box>
                 )
             })
