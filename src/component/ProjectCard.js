eaf7014e504b1392de0fc5d59c2626fddda0c0b4
xavieryn <61944157+xavieryn@users.noreply.github.com>
2022-02-09 11:34:50 -0800

    fixes background image, and adds hover css
    next change is what is inside the boxes

--- src/component/ProjectCard.js
+++ src/component/ProjectCard.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Image, Text, Link} from '@chakra-ui/react';
+import { Box, Heading, Image, Text, Link, AspectRatio} from '@chakra-ui/react';
 import React from 'react';
 import styles from '../../styles/Home.module.css'
 import NextLink from 'next/link';
@@ -15,8 +15,10 @@ const ProjectCard = ( { props }) => {
             flexWrap="wrap"
             flexDirection='row'
             gap="1rem"
-            justifyContent='center'
-            w='100%'
+            //justifyContent='center'
+            // w='calc(100% - 20px)'
+            w="100%"
+            padding='10px'
                      
         >
             {/* {props.items.map((project) => { */}
@@ -24,20 +26,41 @@ const ProjectCard = ( { props }) => {
                 
                 return (
                     // card 
-                    <NextLink href={'youtube.com'} key={project.key} >
-                        <Link>
+                    <NextLink href={'youtube.com'} key={project.key} style={{ textDecoration:'none'}}>
+                    
+                        <Link maxW='100%'  textDecoration='none'>
                             <Box
-                                style = {{ backgroundImage:`url(${"https://berowra.xavier.deta.app" + "/file/" + project.content.ImageGallery.value[0]})`}}
-                                h="40vh"
+                                style = {{ backgroundImage:`url(${"https://berowra.xavier.deta.app" + "/file/" + project.content.ImageGallery.value[0]})`,
+                                aspectRatio:'16/11'
+                                }}
+                                h="auto"   
+                                // maxH='50vh'
                                 display='flex'
-                                w='800px'
-                                maxW='100vw'
+                                w='600px'
+                                maxW='100%'
                                 backgroundSize='cover'
                                 backgroundPosition='center'
                                 backgroundRepeat='no-repeat'
+
                                 
                             > 
-                                
+                                <Box
+                                w='100%'
+                                h='100%'
+                                bgColor='white'
+                                opacity='0'
+                                transition='opacity 0.2s ease-out'
+                                _hover={{ 
+                                    opacity: 1,
+                                    
+                                }}
+                                >
+                                    <Text 
+                                    textAlign='center'
+                                    >
+                                        {project.content.Title.value}
+                                    </Text>
+                                </Box>
                             
                             </Box>
                         </Link>
